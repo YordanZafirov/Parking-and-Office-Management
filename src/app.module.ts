2747498ac4d663396864fa135275d9d2eb379952
Daniel Petrov <daniplpetrov@gmail.com>
2024-02-13 12:43:50 +0200

    feat(GlobalExceptionFilter): add Global Exception Handler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UniqueModule } from './utils/decorators/unique/unique.module';
 import { IsUniqueConstraint } from './utils/decorators/unique/validator';
+import { APP_FILTER } from '@nestjs/core';
+import { GlobalExceptionFilter } from './utils/filters/GlobalExceptionFilter';
 
 @Module({
   imports: [UniqueModule],
   controllers: [],
-  providers: [IsUniqueConstraint],
+  providers: [
+    IsUniqueConstraint,
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
