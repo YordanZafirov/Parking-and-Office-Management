379c316b72fd2e73e4d1809027c0147e3fc439a7
GabrielaNikolova <62817091+GabrielaNikolova@users.noreply.github.com>
2024-02-14 17:26:53 +0200

    feat(create-spot-type.dto.ts): add create spot type dto

---
+++ src/spot-type/dto/create-spot-type.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import { IsUnique } from 'src/utils/decorators/unique/unique.decorator';
+
+export class CreateSpotTypeDto {
+  @IsNotEmpty()
+  @IsUnique({ tableName: 'spot_type', column: 'name' })
+  @IsString({ message: 'Name must be a string' })
+  @MinLength(2, {
+    message: 'Name is too short',
+  })
+  @MaxLength(64, {
+    message: 'Name is too long',
+  })
+  name: string;
+}
