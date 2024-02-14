381795b3422c84451ddb5023ffbe4d486a703b0e
YordanZafirov <iordanzafirov17@gmail.com>
2024-02-14 15:37:52 +0200

    fix(update-user.dto.ts): Fixed newPassword decorator

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,9 +1,7 @@
 import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import {
-  IsNotEmpty,
-  IsStrongPassword,
-} from 'class-validator/types/decorator/decorators';
+import { IsNotEmpty } from 'class-validator';
+import { IsStrongPassword } from 'src/utils/decorators/user-data/password.decorator';
 
 export class UpdateUserDto extends PartialType(
   OmitType(CreateUserDto, ['email'] as const),
