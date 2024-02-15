d7ef4e9701c28139f0758d049c1f993396b74a9b
Nikolay Dimov <dimovv.nikolay@gmail.com>
2024-02-15 13:37:31 +0200

    fix floor-plan create service

--- src/floor-plan/dto/create-floor_plan.dto.ts
+++ src/floor-plan/dto/create-floor_plan.dto.ts
@@ -1,12 +1,9 @@
-import { IsNotEmpty, IsString, IsUUID, Matches } from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 import { IsUnique } from '../../utils/decorators/unique/unique.decorator';
 
 export class CreateFloorPlanDto {
   @IsNotEmpty({ message: 'Name cannot be empty' })
   @IsString({ message: 'Name must be a string' })
-  @Matches(/^[A-Za-z0-9\s\-]+$/, {
-    message: 'Name must contain only letters and numbers',
-  })
   @IsUnique({ tableName: 'floor_plan', column: 'name' })
   name: string;
 
--- src/floor-plan/floor_plan.module.ts
+++ src/floor-plan/floor_plan.module.ts
@@ -3,9 +3,10 @@ import { FloorPlanService } from './floor_plan.service';
 import { FloorPlanController } from './floor_plan.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FloorPlan } from './entities/floor_plan.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FloorPlan])],
+  imports: [TypeOrmModule.forFeature([FloorPlan]), UserModule],
   controllers: [FloorPlanController],
   providers: [FloorPlanService],
   exports: [FloorPlanService],
