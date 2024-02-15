21906e7b7fb314e21a2944c59681ade52e17248a
GabrielaNikolova <62817091+GabrielaNikolova@users.noreply.github.com>
2024-02-15 10:35:17 +0200

    feat(location.entity.ts): add create location dto

--- src/location/dto/create-location.dto.ts
+++ src/location/dto/create-location.dto.ts
@@ -1,1 +1,45 @@
-export class CreateLocationDto {}
+import {
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  MaxLength,
+  IsUUID,
+} from 'class-validator';
+import { IsUnique } from 'src/utils/decorators/unique/unique.decorator';
+
+export class CreateLocationDto {
+  @IsNotEmpty()
+  @IsUnique({ tableName: 'location', column: 'name' })
+  @IsString({ message: 'Name must be a string' })
+  @MinLength(2, {
+    message: 'Name is too short',
+  })
+  @MaxLength(64, {
+    message: 'Name is too long',
+  })
+  name: string;
+
+  @IsNotEmpty()
+  @IsString({ message: 'City must be a string' })
+  @MinLength(2, {
+    message: 'City is too short',
+  })
+  @MaxLength(64, {
+    message: 'City is too long',
+  })
+  city: string;
+
+  @IsNotEmpty()
+  @IsString({ message: 'Address must be a string' })
+  @MinLength(5, {
+    message: 'Address is too short',
+  })
+  @MaxLength(128, {
+    message: 'Address is too long',
+  })
+  address: string;
+
+  @IsNotEmpty()
+  @IsUUID()
+  modifiedBy: string;
+}
