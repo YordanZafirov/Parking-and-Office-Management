13dc897439eb0a1200d9c947b2350bba9c213826
Daniel Petrov <daniplpetrov@gmail.com>
2024-03-05 13:22:21 +0200

    feat(main.ts): change cors origin

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import 'reflect-metadata';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://parking-and-office-management-frontend.vercel.app',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
